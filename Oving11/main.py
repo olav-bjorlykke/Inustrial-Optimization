e86721092806e9d5cda83385ecf50447574e3432
Olav Bjørlykke <olavbjorlykke@dhcp-10-22-58-190.wlan.ntnu.no>
2023-04-21 21:00:11 +0200

    Added functionality for solving the full benders algorithm in main

--- Oving11/main.py
+++ Oving11/main.py
@@ -1,6 +1,31 @@
-import gurobipy as gp
-from gurobipy import GRB, Model
+from sub_problem import SubProblem
+from master_problem import MasterProblem
 
-master_problem = Model ("Master")
+y_values_sub1 = []
+y_values_sub2 = []
+sensitivities_sub1 = []
+sensitivities_sub2 = []
+prev_x_values = []
+solutions = []
+
+for i in range(10):
+    print(f"*********************************         {i}           *********************************")
+    master = MasterProblem(y_values_sub1,y_values_sub2,sensitivities_sub1,sensitivities_sub2,prev_x_values)
+    x_i, solution = master.solve()
+    sub1 = SubProblem(x_i, 1)
+    sub2 = SubProblem(x_i, 1)
+    y_1, sens1 = sub1.solve()
+    y_2, sens2 = sub2.solve()
+
+    y_values_sub1.append(y_1)
+    y_values_sub2.append(y_2)
+    sensitivities_sub1.append(sens2)
+    sensitivities_sub2.append(sens1)
+    prev_x_values.append(x_i)
+
+    solutions.append([i, solution])
+
+print(solutions)
+
 
 
